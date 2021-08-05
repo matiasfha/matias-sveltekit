a99f63d288aed00c93955fe2e8fa591b7793219a
Matias Hernandez <matiasfh@gmail.com>
2021-08-05 16:55:38 -0400

    add redirects

--- web/mdsvex.config.js
+++ web/mdsvex.config.js
@@ -1,6 +1,24 @@
 import headings from "remark-autolink-headings";
 import slug from "remark-slug";
 import  highlight from 'remark-highlight.js';
+import abbr from 'remark-abbr';
+import urls from 'rehype-urls'
+import autoLinkHeadings from 'rehype-autolink-headings'
+
+
+function processUrl(url, node) {
+	if (node.tagName === "a") {
+		node.properties.class = "text-link"
+
+		if (!url.href.startsWith("/")) {
+			// Open external links in new tab
+			node.properties.target = "_blank"
+			// Fix a security concern with offsite links
+			// See: https://web.dev/external-anchors-use-rel-noopener/
+			node.properties.rel = "noopener"
+		}
+	}
+}
 
 const config = {
   "extensions": [".md", '.mdx'],
@@ -11,8 +29,8 @@ const config = {
     "dashes": "oldschool"
   },
 
-  "remarkPlugins": [headings, slug, highlight],
-  "rehypePlugins": []
+  "remarkPlugins": [headings, slug, highlight,, abbr],
+  "rehypePlugins": [[urls, processUrl],[autoLinkHeadings, { behavior: 'wrap'}]]
 };
 
 export default config;
