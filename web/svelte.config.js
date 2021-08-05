5e11f813e1de0fc0702b2c4d3fd1e15ac9beef3a
Matias Hernandez <matiasfh@gmail.com>
2021-08-04 20:24:17 -0400

    fix build. Wrong import

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from 'svelte-preprocess';
-import vercel from 'svelte-adapter'
+import vercel from '@svelte/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
