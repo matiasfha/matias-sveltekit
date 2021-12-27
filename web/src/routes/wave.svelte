0f32442fff09842aa6992ca0d64732e4930b50e8
Matias Hernandez <matiasfh@gmail.com>
2021-12-27 13:13:47 -0300

    remove use off $app/env

--- web/src/routes/wave.svelte
+++ web/src/routes/wave.svelte
@@ -1,51 +1,43 @@
 <script lang="ts">
+	import { useWeb3 } from 'svweb3';
+
+	const { connect, subscribe } = useWeb3({
+		init: async () => {
+			await setupContract();
+			await getAllWaves();
+		}
+	});
 	import CtaButton from '$components/CtaButton.svelte';
 	import { ethers } from 'ethers';
-	import { onDestroy, onMount } from 'svelte';
+	import { onDestroy } from 'svelte';
 	import abi from '../artifacts/src/contracts/WavePortal.sol/WavePortal.json';
 	const contractAddress = '0xE48FD418B5970C3Ba7430014B44CcB4168498bd7';
-	let currentAccount = null;
+
 	let totalCount = 0;
 	$: waiving = false;
 	let allWaves = [];
 	let message;
 	let contract;
 
-	const checkIfWalletIsConnected = async () => {
-		try {
-			const { ethereum } = window;
-			if (!ethereum) {
-				alert('There is no wallet!');
-				return null;
-			}
-			const accounts = await ethereum.request({ method: 'eth_accounts' });
-			if (accounts != null && Array.isArray(accounts)) {
-				currentAccount = accounts[0];
-			} else {
-				console.log('No accounts found');
-			}
-		} catch (error) {
-			console.error(error);
-		}
-	};
+	let currentAccount = null;
+	let isConnected = false;
+	let signer = null;
+	let ens = null;
+	let hasPermissions = false;
+	let network = null;
+	let balance = 0;
 
-	const connectWeb3 = async () => {
-		try {
-			const { ethereum } = window;
-			if (!ethereum) {
-				alert('You need a wallet like Metamask');
-			}
-			const accounts = await ethereum.request({ method: 'eth_accounts' });
-			if (accounts != null && Array.isArray(accounts)) {
-				currentAccount = accounts[0];
-				console.log('Connected to: ', currentAccount);
-			} else {
-				console.log('No accounts found');
-			}
-		} catch (error) {
-			console.error(error);
-		}
-	};
+	subscribe(async (state) => {
+		({
+			account: currentAccount,
+			isConnected,
+			hasPermissions,
+			signer,
+			ens,
+			network,
+			balance
+		} = state);
+	});
 
 	const onNewWave = (from, timestamp, message) => {
 		allWaves = [
@@ -62,30 +54,24 @@
 		alert('You win the prize!!!');
 	};
 
-	const setupContract = async () => {
-		const { ethereum } = window;
-		await checkIfWalletIsConnected();
-		if (ethereum && currentAccount) {
-			// @ts-ignore
-			const provider = new ethers.providers.Web3Provider(ethereum);
-			const signer = provider.getSigner();
+	async function setupContract() {
+		if (window.ethereum && isConnected) {
 			contract = new ethers.Contract(contractAddress, abi.abi, signer);
 			contract.on('NewWave', onNewWave);
 			contract.on('NewPrize', onPrize);
 		}
-	};
+	}
 
 	const wave = async () => {
 		try {
-			const { ethereum } = window;
-			if (ethereum && currentAccount) {
+			if (isConnected && contract) {
 				waiving = true;
 				/* Execute the contract */
 				const waveTxn = await contract.wave(message, { gasLimit: 300000 });
 				await waveTxn.wait();
 				message = '';
 			} else {
-				console.log('Not reading', { ethereum, currentAccount });
+				console.log('Not reading', { currentAccount: currentAccount });
 				return null;
 			}
 		} catch (error) {
@@ -97,10 +83,9 @@
 		}
 	};
 
-	const getAllWaves = async () => {
+	async function getAllWaves() {
 		try {
-			const { ethereum } = window;
-			if (ethereum) {
+			if (isConnected && contract) {
 				const waves = await contract.getAllWaves();
 
 				allWaves = waves.map((item) => {
@@ -117,12 +102,8 @@
 		} catch (error) {
 			console.error(error);
 		}
-	};
+	}
 
-	onMount(async () => {
-		await setupContract();
-		await getAllWaves();
-	});
 	onDestroy(() => {
 		contract?.off('NewWave', onNewWave);
 	});
@@ -137,29 +118,37 @@
 		El smart contract está disponible en <strong>Rinkeby</strong>.
 	</p>
 
-	{#if currentAccount == null}
-		<button on:click={connectWeb3} class="p-8 text-lg bg-green-400 rounded-md w-60 mx-auto"
+	{#if !isConnected}
+		<button on:click={connect} class="p-8 text-lg bg-green-400 rounded-md w-60 mx-auto"
 			>Connect to Web3</button
 		>
 	{:else}
+		{#if !hasPermissions}
+			<button on:click={connect} class="p-8 text-lg bg-green-400 rounded-md w-60 mx-auto"
+				>Obtener Permisos</button
+			>
+		{/if}
 		<div>
-			<div>
-				<input
-					type="text"
-					bind:value={message}
-					class="border-secondary hover:border-primary focus:border-primary focus:bg-secondary px-8 py-5 w-full dark:text-white bg-transparent border rounded-lg focus:outline-none disabled:cursor-not-allowed"
-					placeholder="Tu Saludo"
-					disabled={waiving}
-				/>
-				{#if waiving}
-					<CtaButton text={`Mining the wave...`} onClick={wave} />
-				{:else}
-					<CtaButton
-						text={`Envíame un crypto saludo 👋 Total waves until now: ${totalCount}`}
-						onClick={wave}
+			{#if hasPermissions}
+				<div>
+					<input
+						type="text"
+						bind:value={message}
+						class="border-secondary hover:border-primary focus:border-primary focus:bg-secondary px-8 py-5 w-full dark:text-white bg-transparent border rounded-lg focus:outline-none disabled:cursor-not-allowed"
+						placeholder="Tu Saludo"
+						disabled={waiving}
 					/>
-				{/if}
-			</div>
+					{#if waiving}
+						<CtaButton text={`Mining the wave...`} onClick={wave} />
+					{:else}
+						<CtaButton
+							text={`Envíame un crypto saludo 👋 Total waves until now: ${totalCount}`}
+							onClick={wave}
+							disabled={waiving}
+						/>
+					{/if}
+				</div>
+			{/if}
 			<table
 				class="table-auto mt-12 border-collapse border p-2  mb-12 rounded-md
         dark:bg-gray-200 
@@ -185,7 +174,13 @@
 				</tbody>
 			</table>
 			<h2 class="text-gray-600 dark:text-white text-md text-center font-body m-0 p-0">
-				Connected to Web3: {currentAccount}
+				Connected to Web3:
+				<ul>
+					<li>Address:{currentAccount}</li>
+					{#if ens} <li>ens: {ens}</li>{/if}
+					<li>Network: {network.name}</li>
+					<li>Balance: {balance}</li>
+				</ul>
 			</h2>
 		</div>
 	{/if}
