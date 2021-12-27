7ee9cc9cdfc25bb4a7f16316e9602d86fd9be209
Matias Hernandez <matiasfh@gmail.com>
2021-12-27 13:13:47 -0300

    remove use off $app/env

--- web/src/lib/stores/index.ts
+++ web/src/lib/stores/index.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 const createWritableStore = (key: string, startValue: string | Record<string, unknown>) => {
 	const { subscribe, set } = writable(startValue);
 
