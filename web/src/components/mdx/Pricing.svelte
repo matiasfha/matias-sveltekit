80f9ae642e46e9b6acb4331297375f4211342973
Matias Hernandez <matiasfh@gmail.com>
2022-06-28 19:56:49 -0400

    missing pricing translation

--- web/src/components/mdx/Pricing.svelte
+++ web/src/components/mdx/Pricing.svelte
@@ -1,11 +1,15 @@
-<div class=" border-purple rounded-md border-t-4 w-2/4 mt-0 mx-auto px-12 py-8 flex flex-col items-center justify-center shadow-md mb-20 dark:bg-ebony-clay-500" >
-    <h3 class="p-0 !my-0">Paquete semanal</h3>
+<script lang="ts">
+    import { t } from '$lib/translations';
 
-    <p class="text-5xl !mb-0 !py-4">$50 <strong>USD</strong></p>
+</script>
+<div class=" border-purple rounded-md border-t-4 w-2/4 mt-0 mx-auto px-12 py-8 flex flex-col items-center justify-center shadow-md mb-20 dark:bg-ebony-clay-500" >
+    <h3 class="p-0 !my-0">{$t('common.pricing.title')}</h3>
+
+    <p class="text-5xl !mb-0 !py-4">$100 <strong>USD</strong></p>
 
     <ul class="font-body !m-0 !p-0 w-full">
-        <li> ✅ Una semana de publicidad exclusiva</li>
-        <li> ✅ Incluye diseño y redacción</li>
+        <li> ✅ {$t('common.pricing.exclusivity')}</li>
+        <li> ✅ {$t('common.pricing.includes')}</li>
     </ul>
 
     <a href="mailto:sponsor@matiashernandez.dev" class="bg-purple hover:bg-purple-500 p-4 !text-gray-100 rounded-md">Contactar a ventas</a>
