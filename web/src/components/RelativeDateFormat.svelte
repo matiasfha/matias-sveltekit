4620979c83d5216afa3a3ae0e42987fce54f3912
Matias Hernandez <matiasfh@gmail.com>
2022-09-19 06:52:55 -0300

    use pnpm on netlify

---
+++ web/src/components/RelativeDateFormat.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { locale } from '$lib/translations';
+    export let date: string
+    $: currentLocale = 'en'
+    locale.subscribe((str) => {
+        currentLocale = str
+    })
+    const diffDate = new Date().getTime() - new Date(date).getTime()
+    const diff = Math.floor(diffDate / (1000*60*60*24))
+    $: rdf = new Intl.RelativeTimeFormat(currentLocale)
+    $: displayDate = rdf.format(-diff, 'days')
+</script>
+
+{displayDate}
