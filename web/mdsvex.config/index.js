d504107efbc4f983d70c2be45b1f8777707f7b15
Matias Hernandez <matiasfh@gmail.com>
2022-11-12 12:45:39 -0300

    add table of contents

--- web/mdsvex.config/index.js
+++ web/mdsvex.config/index.js
@@ -1,7 +1,6 @@
 
 
 import urls from 'rehype-urls'
-import autoLinkHeadings from 'rehype-autolink-headings'
 import 'dotenv/config'
 import { highlighterFn } from './highlighterFn.js';
 import { cloudinaryImages } from './cloudinaryImages.js';
@@ -10,6 +9,32 @@ import { remmarkPath } from './remmarkPath.js';
 import { remarkSponsor } from './remarkSponsor.js';
 import { processUrl } from './processUrl.js';
 import { setDefaultLang } from './setDefaultLang.js';
+import { visit } from 'unist-util-visit';
+import {toString} from 'mdast-util-to-string'
+import slug from '@microflash/rehype-slugify'
+import Slugger from 'github-slugger'
+import jsyaml from 'js-yaml'
+const slugger = new Slugger()
+
+export function fmToc() {
+	slugger.reset()
+	return async function (node, vFile) {
+		let toc = []
+		visit(node, ['heading'], (node) => {
+			if(node.depth <= 3) {
+				const value = toString(node, { includeImageAlt: false})
+				const slug = slugger.slug(value)
+				toc.push({ slug, value})
+			}
+		})
+		if (!vFile.data.fm) {
+			vFile.data.fm = {};
+		}
+		if (!vFile.data.fm.toc) {
+			vFile.data.fm.toc = jsyaml.dump(JSON.stringify(toc))
+		}
+	};
+}
 
 /**
  * @type { import('mdsvex').MdsvexOptions}
@@ -28,8 +53,16 @@ const config = {
 		highlighter: highlighterFn
 	},
 
-	"remarkPlugins": [remarkReadingTime, remarkSponsor, remmarkPath, cloudinaryImages, setDefaultLang],
-	"rehypePlugins": [[urls, processUrl],  [autoLinkHeadings, { behavior: 'wrap' }]]
+	"remarkPlugins": [remarkReadingTime, remarkSponsor, remmarkPath, cloudinaryImages, setDefaultLang, fmToc],
+	"rehypePlugins": [[urls, processUrl],  [slug, {
+		reset() {
+			slugger.reset()
+		  },
+		  slugify(text) {
+			return slugger.slug(text)
+		  }
+		}]
+	]
 };
 
 export default config
