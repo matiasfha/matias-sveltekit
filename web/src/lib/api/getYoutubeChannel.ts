4f310523d82cf90b488ad5d3d963f5a86b0f1aeb
Matias Hernandez <matiasfh@gmail.com>
2022-12-20 23:21:07 -0300

    use process.env

--- web/src/lib/api/getYoutubeChannel.ts
+++ web/src/lib/api/getYoutubeChannel.ts
@@ -2,9 +2,9 @@
 import z from 'zod'
 
 const VideoId = z.discriminatedUnion('kind', [
-  z.object({ kind: z.literal('youtube#video'), videoId: z.string()}),
-  z.object({ kind: z.literal('youtube#playlist'), playlistId: z.string()}),
-  z.object({ kind: z.literal('youtube#channel'), channelId: z.string()}),
+  z.object({ kind: z.literal('youtube#video'), videoId: z.string() }),
+  z.object({ kind: z.literal('youtube#playlist'), playlistId: z.string() }),
+  z.object({ kind: z.literal('youtube#channel'), channelId: z.string() }),
 ])
 
 const Video = z.object({
@@ -22,26 +22,26 @@ const Video = z.object({
     })
   })
 })
-.transform(args => ({
-  id: args.id.kind == 'youtube#video' ? args.id.videoId : args.id.kind === 'youtube#playlist' ? args.id.playlistId : args.id.channelId,
-  kind: args.id.kind,
-  title: args.snippet.title,
-  publishedAt: args.snippet.publishedAt,
-  description: args.snippet.description,
-  thumb: args.snippet.thumbnails.high
-}))
+  .transform(args => ({
+    id: args.id.kind == 'youtube#video' ? args.id.videoId : args.id.kind === 'youtube#playlist' ? args.id.playlistId : args.id.channelId,
+    kind: args.id.kind,
+    title: args.snippet.title,
+    publishedAt: args.snippet.publishedAt,
+    description: args.snippet.description,
+    thumb: args.snippet.thumbnails.high
+  }))
 
 export const Videos = z.array(Video)
 
-const API_KEY = import.meta.env.YOUTUBE_API_KEY
+const API_KEY = process.env.YOUTUBE_API_KEY
 export async function getVideos() {
   try {
-  const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=UChYjCtGvtgzh7HvJuanV1-g&part=snippet,id&order=date&maxResults=100`)
-  const data = await res.json().then(r => {
-    return  Videos.parse(r.items)
+    const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=UChYjCtGvtgzh7HvJuanV1-g&part=snippet,id&order=date&maxResults=100`)
+    const data = await res.json().then(r => {
+      return Videos.parse(r.items)
     })
-  return data.filter(item => item.kind === 'youtube#video')
-  }catch(e) {
+    return data.filter(item => item.kind === 'youtube#video')
+  } catch (e) {
     console.error(e)
     return []
   }
