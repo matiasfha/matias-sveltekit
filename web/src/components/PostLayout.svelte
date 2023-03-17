998f32677a1ba25a9c9a2a0b67929bf428691b88
Matias Hernandez <matiasfh@gmail.com>
2023-03-17 06:39:17 -0300

    move script to layout

--- web/src/components/PostLayout.svelte
+++ web/src/components/PostLayout.svelte
@@ -9,7 +9,7 @@
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-	import { afterUpdate, onMount } from 'svelte';
+	import { afterUpdate } from 'svelte';
 	import Seo from './Seo.svelte';
 	import RelativeDateFormat from './RelativeDateFormat.svelte';
 
@@ -40,39 +40,9 @@
 		currentUrl = window.location.href;
 	});
 
-	let adsbygoogle = [{}];
-	onMount(() => {
-		if (window.adsbygoogle) {
-			adsbygoogle = (window.adsbygoogle || []).push({});
-		}
-	});
 	const newsletterId = lang === 'es' ? 'c4r8t8' : 'h7k7g0';
 </script>
 
-<svelte:head>
-	<script src="https://f.convertkit.com/ckjs/ck.5.js" type="text/partytown"></script>
-	<script
-		async
-		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8352667732450998"
-		crossorigin="anonymous"
-		defer
-	></script>
-	<script
-		type="text/partytown"
-		data-name="BMC-Widget"
-		data-cfasync="false"
-		src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-		data-id="matiasfha"
-		data-description="Support me on Buy me a coffee!"
-		data-message="If you find the content useful, would you like to invite me for coffee?Thanks for reading!"
-		data-color="#5F7FFF"
-		data-position="Right"
-		data-x_margin="18"
-		data-y_margin="18"
-		async
-		defer
-	></script>
-</svelte:head>
 
 <Seo {title} {description} {keywords} isBlogPost={true} {canonical} {banner} />
 <header
--- web/src/routes/blog/post/+layout.svelte
+++ web/src/routes/blog/post/+layout.svelte
@@ -1,4 +1,38 @@
 <script>
+	import { onMount } from "svelte";
+
+
+	let adsbygoogle = [{}];
+	onMount(() => {
+		if (window.adsbygoogle) {
+			adsbygoogle = (window.adsbygoogle || []).push({});
+		}
+	});
     
 </script>
+
+<svelte:head>
+	<script
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8352667732450998"
+		crossorigin="anonymous"
+		defer
+	></script>
+	<script src="https://f.convertkit.com/ckjs/ck.5.js" type="text/partytown"></script>
+	<script
+		type="text/partytown"
+		data-name="BMC-Widget"
+		data-cfasync="false"
+		src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+		data-id="matiasfha"
+		data-description="Support me on Buy me a coffee!"
+		data-message="If you find the content useful, would you like to invite me for coffee?Thanks for reading!"
+		data-color="#5F7FFF"
+		data-position="Right"
+		data-x_margin="18"
+		data-y_margin="18"
+		async
+		defer
+	></script>
+</svelte:head>
 <slot></slot>
