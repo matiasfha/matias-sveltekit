a4bfc664cc1007ae212f4d0a82bffdcef7250a50
Matias Hernandez <matiasfh@gmail.com>
2023-04-16 09:55:41 -0400

    fix newsletter

--- web/src/routes/newsletter/+page.server.ts
+++ web/src/routes/newsletter/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import z from 'zod';
 import { client, builder } from '$lib/utils/sanityClient';
 import { env } from '$env/dynamic/private';
@@ -24,25 +24,16 @@ export const load: PageServerLoad = async () => {
 		courses: courses.map((item) => ({ ...item, image: builder.image(item.image).url() }))
 	};
 };
-type ConvertKitSubscriber = {
-  id: number
-  first_name: string
-  email_address: string
-  state: 'active' | 'inactive'
-  created_at: string
-  fields: Record<string, string | null>
-}
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
 		const email = data.get('email');
 		const sequenceId = data.get('sequenceId')
-		const tags = data.get('tags')
 		const subscriberData = {
 			api_key: env.CONVERT_KIT_API_KEY,
 			api_secret: env.CONVERT_KIT_API_SECRET,
 			email,
-			tags: [...tags, 'Spanish']
+			tags: ['Spanish']
 		};
 		const response = await fetch(
 			`https://api.convertkit.com/v3/sequences/${sequenceId}/subscribe`,
@@ -60,4 +51,4 @@ export const actions = {
 			data: json.subscription
 		}
 	}
-};
+} satisfies Actions;
